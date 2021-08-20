931c72d12a9d32682f3a709d4983ba0c91262b1d
RompallyAnuradha <anuradharompally@gmail.com>
2021-08-20 09:37:04 +0530

    updated project

--- Udemy-React/src/Components/Cart.js
+++ Udemy-React/src/Components/Cart.js
@@ -5,7 +5,6 @@ export default function Cart(props) {
     console.log("props" , props)
     const itemsPrice=cartItems.reduce((a,c)=> a + c.price * c.qty, 0 );
     const taxPrice= itemsPrice * 0.14;
-    
     const totalPrice = itemsPrice + taxPrice 
     
     return (
